8f19fb79ac6f034de74bca3be3980c3778ac278e
João Alberto Redondo <169093784+joaoalbertoredondo@users.noreply.github.com>
2025-02-06 14:29:00 -0300

    feat: add deletion functionality for questions and answers

---
+++ test/factories/make-answer.ts
@@ -0,0 +1,20 @@
+import { faker } from '@faker-js/faker'
+
+import { UniqueEntityId } from '@/core/entities/unique-entity-is'
+import { Answer, AnswerProps } from '@/domain/forum/enterprise/entities/answer'
+
+export function makeAnswer(
+  override: Partial<AnswerProps> = {},
+  id?: UniqueEntityId,
+) {
+  const answer = Answer.create(
+    {
+      authorId: new UniqueEntityId(),
+      questionId: new UniqueEntityId(),
+      content: faker.lorem.text(),
+      ...override,
+    },
+    id,
+  )
+  return answer
+}
